071ba8b277b864df5916e612b329e90f7fe0514d
pabparmen <pabparmen@alum.us.es>
2023-11-27 10:17:29 +0100

    register

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
+    path('', include('django.contrib.auth.urls')),
+   # path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
+#from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -24,3 +25,19 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['contraseña'])
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    
+    return render(request, 'account/register.html', {'user_form': user_form})
+
+
