c1e728d40ddbe80d7bda26a2de086957d0d332ed
alisanhos <alisanhos@alum.us.es>
2023-11-28 21:47:17 +0100

    Cambio con la URL

--- carplay_Peralta/urls.py
+++ carplay_Peralta/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('', include('shop.urls', namespace='shop')),
+    path('shop/', include('shop.urls', namespace='shop')),
 ]
 
 if settings.DEBUG:
