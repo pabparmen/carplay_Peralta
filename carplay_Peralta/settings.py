b915215b512f77f7e8fb7972719455d90d1ae38e
Samalbalat <saoalbalat@gmail.com>
2023-12-01 15:25:41 +0100

    Carrito de la compra listo para revisión
    Este commit también altera ciertos archivos del módulo de tienda
    
    Requiere revisión

--- carplay_Peralta/settings.py
+++ carplay_Peralta/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# Cart variable for Settings
+CART_SESSION_ID = 'cart'
 
 ROOT_URLCONF = 'carplay_Peralta.urls'
 
@@ -64,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
